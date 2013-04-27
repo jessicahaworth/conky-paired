1524b4fe90820852e4f74405eb44fda3ae396a69
Jessica Haworth <jess@catdice.com>
2013-04-26 23:56:48 -0500

    button now switches between set of script/pic pairs

--- src/main/java/com/catdice/code/ConkyPaired.java
+++ src/main/java/com/catdice/code/ConkyPaired.java
@@ -1,6 +1,11 @@
 package com.catdice.code;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -36,15 +41,25 @@ public class ConkyPaired {
 				if (toggle) {
 					fileLoc = "pics/2.jpg";
 				}
+
+				File picFile = new File(fileLoc);
+
+				fileLoc = "scripts/1_conkyrc";
+				if (toggle) {
+					fileLoc = "scripts/2_conkyrc";
+				}
+
+				File conkyFile = new File(fileLoc);
 				toggle = !toggle;
-				File file = new File(fileLoc);
 				try {
 					String desktopSession = System.getenv("DESKTOP_SESSION");
-					setWallpaper(file, desktopSession);
-				} catch (Exception e1) {
-					System.err.println("Error! " + e.toString());
+					setWallpaper(picFile, desktopSession);
+					runConkyScript(conkyFile);
+				} catch (IOException ex) {
+					System.err.println("Error! " + ex.toString());
 				}
 			}
+
 		});
 		shell.setDefaultButton(ok);
 		shell.setLayout(new RowLayout());
@@ -58,7 +73,8 @@ public class ConkyPaired {
 	}
 
 	// original gconftool-2 and /desktop/gnome/...
-	public void setWallpaper(File file, String desktopSession) throws Exception {
+	public void setWallpaper(File file, String desktopSession)
+			throws IOException {
 		String[] script;
 		if (desktopSession.equals("mate")) {
 			String s[] = { "mateconftool-2", "-t", "string", "-s",
@@ -72,6 +88,39 @@ public class ConkyPaired {
 			script = s;
 		}
 		Runtime runtime = Runtime.getRuntime();
-		runtime.exec(script);
+		if (file.exists()) {
+			runtime.exec(script);
+		}
+	}
+
+	private void runConkyScript(File file) throws IOException {
+		// String[] scriptKillConky = { "/usr/bin/pkill", "'^conky$'" };
+		Runtime runtime = Runtime.getRuntime();
+
+		String[] scriptKillConky = { "/usr/bin/pkill", "conky" };
+		String[] scriptFindConky = { "/usr/bin/pgrep", "conky" };
+
+		Process proc = runtime.exec(scriptFindConky); // find running instance
+														// of conky, if
+		InputStream stdin = proc.getInputStream();
+		InputStreamReader isr = new InputStreamReader(stdin);
+		BufferedReader br = new BufferedReader(isr);
+		String line = null;
+		ArrayList<String> runningConkys = new ArrayList<String>();
+		while ((line = br.readLine()) != null) {
+			runningConkys.add(line);
+		}
+
+		for (String process : runningConkys) {
+			String[] killConky = { "/bin/kill", process };
+			runtime.exec(killConky);
+		}
+
+		if (file.exists() && file.isFile()) {
+			String[] scriptRunConky = { "conky", "-c", file.getAbsolutePath() };
+			// runtime.exec(scriptKillConky); // kill running instance of conky
+			runtime.exec(scriptRunConky); // run conky with new conkyrc
+		}
+
 	}
 }
